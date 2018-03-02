cec7c9474b2725b09108d46a632bb819a89922ed
Victor A Chavez <v.a.ch.m@hotmail.com>
2018-03-02 12:30:37 -0800

    Fist Commit

---
+++ CustomeDbContext/CustomeDbContext/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomeDbContext
+{
+    public class Class1
+    {
+    }
+}
